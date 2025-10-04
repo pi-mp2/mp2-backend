681907c262c51932a2131abec0b6c6c78e8a444c
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2025-10-03 22:28:04 -0500

    Implementing password recovery with email and reset with JWT

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,17 +1,20 @@
 import { Router } from "express";
 import { register } from "@controllers/authController";
+import { login } from "@controllers/authController";
+import { requestPasswordReset, resetPassword } from "@controllers/authController";
 
 const router = Router();
 
 router.post("/register", register);
 
 // dejamos login y logout como placeholder por ahora
-router.post("/login", (req, res) => {
-  res.json({ message: "Login endpoint 🚧" });
-});
+router.post("/login", login);
 
 router.post("/logout", (req, res) => {
   res.json({ message: "Logout endpoint 🚧" });
 });
+
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/reset-password", resetPassword);
 
 export default router;
