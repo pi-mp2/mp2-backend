31a0fb7f3eb354f437c9fed4f55133be128df288
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2025-10-17 18:16:58 -0500

    Configuracion cors para render

--- src/index.ts
+++ src/index.ts
@@ -27,14 +27,7 @@ const allowedOrigins= [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
+    origin: "*",
   })
 );
 
